56f730bf71c682c404dd15df2cc6731bfa9b8bc4
STMIK BANDUNG <developer.stmikbandung@gmail.com>
2025-05-05 20:44:25 +0700

    dospem

--- resources/views/contents/roles.blade.php
+++ resources/views/contents/roles.blade.php
@@ -60,12 +60,12 @@
                         'img' => '/images/leader.png'
                     ];
                     break;
-		case 'is_dospem':
-		    $setRoles['Dosen Pembimbing']=[
-			'role' => $role,
-			'img' => '/images/dosen.jpeg'
-		   ];
-		   break;
+		        case 'is_dospem':
+		            $setRoles['Dosen Pembimbing']=[
+			            'role' => $role,
+			            'img' => '/images/dosen.jpeg'
+		            ];
+		            break;
                 default:
                     break;
             }
